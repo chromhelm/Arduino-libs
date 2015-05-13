443163d7634ff15bff9c0ce74324a7a93dc05764
chromhelm <chromhelm@users.noreply.github.com>
2015-05-13 21:47:22 +0200

    Create PCF8577C.h

---
+++ PCF8577C/PCF8577C.h
@@ -0,0 +1,22 @@
+#ifndef PCF8577C_H
+#define PCF8577C_H
+
+#include <Arduino.h>
+#include <Wire.h>
+
+extern TwoWire Wire;
+
+/* arduino class */
+class PCF8577C {
+    public:
+        static const uint8_t DIRECT_DRIVE = 0x0;
+        static const uint8_t DUPLEX_DRIVE = 0x1;
+        
+        void begin(uint8_t DriveMode, uint8_t subaddress);
+        void selectBank(uint8_t bankNumber);
+        void write(uint8_t* data, uint8_t length);
+    private:
+    	uint8_t ControlRegister;
+};
+
+#endif //PCF8577C_H
